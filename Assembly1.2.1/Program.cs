c06bbfe8d2c57f0b060e5eadf2e24dfb686038d8
Daniela <Daniela@DESKTOP-P4BHM2A>
2023-07-20 17:17:34 +0100

    Adicionar arquivos de projeto.

---
+++ Assembly1.2.1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Assembly1._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escreva quatro números");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            int number4 = int.Parse(Console.ReadLine());
+
+            if (number1 >= number2 && number1 >= number3 && number1 >= number4)
+            {
+                Console.WriteLine($"O número maior é o {number1}");
+            }
+            else if (number2 >= number1 && number2 >= number3 && number2 >= number4)
+            {
+                Console.WriteLine($"O número maior é o {number2}");
+
+            }
+            else if (number3 >= number1 && number3 >= number2 && number3 >= number4)
+            {
+                Console.WriteLine($"O número maior é o: {number3}");
+
+            }
+            else
+            {
+                Console.WriteLine($"O número ´maior é o: {number4}");
+            }
+
+        }
+    }
+}
